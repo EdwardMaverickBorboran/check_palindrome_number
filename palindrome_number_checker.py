1173b1be691b7ed2cb6cab9ad74fff846084cfd4
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 20:38:26 +0800

    I added a input function to get the string from the user.

---
+++ palindrome_number_checker.py
@@ -0,0 +1,20 @@
+# Create a program that will --
+
+# Check if the given number is a palindrome number.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+# Expected results:
+
+# original number 121
+# Yes. given number is palindrome number
+
+# original number 125
+# No. given number is not palindrome number
+
+# Asking the user to input a number
+given_number = int(input("Enter a number: "))
+print("Original number is: ", given_number)
