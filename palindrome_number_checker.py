4f531f505eb56533d900e98227d6f8c244eb92cd
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-22 21:53:42 +0800

    I tried converting the variable (given_number) into string. Yet it is still detected as not a Palindrome number.

--- palindrome_number_checker.py
+++ palindrome_number_checker.py
@@ -16,14 +16,15 @@
 # No. given number is not palindrome number
 
 # Asking the user to input a number
-given_number = int(input("Enter a number: "))
-print("Original number is:", given_number)
+given_number = (input("Enter a number: "))
+print("The original number is:", given_number)
 
 # Using reverse function to reverse the given number 
-reverse_number = reversed(given_number)
+given_number_str = str(given_number)
+reverse_number = reversed(given_number_str)
 
 # Checking if the given number is Palindrome number
-if given_number == reverse_number:
+if given_number_str == reverse_number:
     print("The given number is a Palindrome number")
 else:
     print("The given number is not a Palindrome number")
